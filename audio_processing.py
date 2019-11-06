4370be29ec04c4bd7c8e4a2b699d3f498e544efb
SalomeWairimu <salomekariuki2020@u.northwestern.edu>
2019-11-06 14:42:23 -0600

    end to end

---
+++ audio_processing.py
@@ -0,0 +1,83 @@
+import json
+import requests
+import time
+import pygame
+from gtts import gTTS
+from difflib import SequenceMatcher
+import sys
+import math
+import struct
+import time
+import csv
+import pyaudio
+import wave
+import speech_recognition as sr
+
+from text_similarity import get_best_answer_audio_id
+
+def play_sound(fname):
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(fname)
+    print("playing", fname)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    print("done playing", fname)
+
+def play_piece_audios(audios):
+    for audio in audios:
+        play_sound(audio)
+
+def record_qst():
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 8
+    WAVE_OUTPUT_FILENAME ="question.wav"
+    
+    audio = pyaudio.PyAudio()
+    
+    # start Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    print("recording...")
+    frames = []
+    
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("finished recording")
+    
+    
+    # stop Recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+
+def get_qst(fname):
+    audio_f = sr.AudioFile(fname)
+    r = sr.Recognizer()
+    with audio_f as source:
+        audio = r.record(source)
+    try:
+        qst = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        qst = None
+    return qst
+
+
+def play_answer(qst):
+    audio_id = get_best_answer_audio_id(qst)
+    # audio_file = retrieveAudioById(audio_id)
+    audio_file = "crucifixion3.wav"
+    play_sound(audio_file)
--- text_similarity.py
+++ text_similarity.py
@@ -9,14 +9,14 @@ def retrieve_audio_texts():
     piId = hex(uuid.getnode())
     print(piId)
     # audio_texts = retrieveByRasberryPiId(piId)
-    audio_texts = [
+    audio_ids, audio_texts = [0,1,2,3,4], [
         "The crucifixion was created in the 14th century  circa 1350-1359 experts are not sure exactly when but it is estimated that it was around 1352",
         "honestly this did not stand out much compared to the many depictions of the crucifixion the only original part was the swan in the birds overlooking it as Christ is supposed to",
         "what strikes me about the crucifixion is that some idiot tried to buy this for 100 Bitcoin that's almost $800,000 absolutely insane I don't understand it",
         "what's really interesting to me about this crucifixion painting is that there is this little bird with a nest on the top and I keep trying to figure out what that's trying to symbolize is the bird God and he is guarding over his children by sending his one and only son Jesus Christ to die for our sins or is the bird Mary Magdalene who's also you know that her son go off and die for this purpose so that's that's a really interesting question for me",
         "the crucifixion is based on a wood panel with gold leaf and be inscribed images that you see the gold brings out the Holiness and spirituality associated with this imagery and prepares it for its placement in a high-end esteem Church",
     ]
-    return audio_texts
+    return audio_ids, audio_texts
 
 # Clean up text by removing stop words and words that may not be relevant.
 def clean_audio_texts(documents):
@@ -45,9 +45,9 @@ def create_lsi_model(texts):
     lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=2)
     return dictionary, corpus, lsi
 
-def get_best_answer(question):
+def get_best_answer_audio_id(question):
     # Retrieve all texts related to the current art piece.
-    audio_texts = retrieve_audio_texts()
+    audio_ids, audio_texts = retrieve_audio_texts()
     texts = clean_audio_texts(audio_texts)
     dictionary, corpus, lsi = create_lsi_model(texts)
     vec_bow = dictionary.doc2bow(question.lower().split())
@@ -58,7 +58,8 @@ def get_best_answer(question):
     sims = index[vec_lsi]  # perform a similarity query against the corpus
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
     #print(sims)
-    for i, s in sims:
-        print(s, audio_texts[i])
+    # for i, s in sims:
+    #     print(s, audio_texts[i])
+    return audio_ids[sims[0][0]]
 
-# get_best_answer("when was it created?")
+# get_best_answer_audio_id("what strikes you about it?")
